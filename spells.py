77e9ada79404a8ebfabc52ecbd5d78aaf764d4fc
phmnsx <lynnwastinghertime@gmail.com>
2024-04-23 20:54:52 -0300

    created spells.py

---
+++ spells.py
@@ -0,0 +1,11 @@
+import json
+
+def get_spell(id):
+    f = open('static/spells.json', encoding="utf8")
+    spells = json.load(f)
+    f.close
+    for spell in spells:
+        if spells["ID"] == id:
+            return spell
+    else:
+        return "Error 11: Spell not found."
